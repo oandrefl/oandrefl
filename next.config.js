3abba29ac507e6ac7555b7679d1a69cb2b0e5af6
Andre Fernando Lara <andrefernandolara@hotmail.com>
2026-02-16 19:30:36 -0300

    corrigindo nwxt config

--- next.config.js
+++ next.config.js
@@ -1,10 +1,17 @@
-const withMDX = require('@next/mdx')()
+const withMDX = require('@next/mdx')({
+  extension: /\.mdx?$/,
+  options: {
+    remarkPlugins: [],
+    rehypePlugins: [],
+  },
+})
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
   experimental: {
-    // mdxRs: true, // DESATIVE ESTA LINHA (Comente ou remova)
+    // mdxRs: true, // Mantemos desativado por enquanto para evitar erros de React antigo
+    turbopack: {}, // <--- ADICIONE ESTA LINHA PARA RESOLVER O ERRO DE BUILD
   },
 }
 
